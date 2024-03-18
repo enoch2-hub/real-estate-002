d3829564129b26f41a5771b0cab69b8e09640d4c
enoch2-hub <enoch.perera.coding2@gmail.com>
2024-03-18 23:29:46 +0530

    fix responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,41 +1,52 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-import {FiGitPullRequest} from 'react-icons/fi'
-import {FaRegTimesCircle} from 'react-icons/fa'
-import {HiOutlineMenuAlt4} from 'react-icons/hi'
+import { FiGitPullRequest } from "react-icons/fi";
+import { FaRegTimesCircle } from "react-icons/fa";
+import { HiOutlineMenuAlt4 } from "react-icons/hi";
 
-import './Navbar.css'
+import "./Navbar.css";
 
 const Navbar = () => {
+  const [click, setClick] = useState(false);
 
-    const [click, setClick] = useState(false)
-
-    const toggleMenu = () => setClick(!click)
-
-
+  const toggleMenu = () => setClick(!click);
 
   return (
-    <div className='navbar'>
-        <div className="container">
-            <h1><span><FiGitPullRequest/>Text</span>TExt</h1>
-            <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Search</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Contact</a></li>
-            </ul>
+    <div className="navbar">
+      <div className="container">
+        <h1>
+          <span>
+            <FiGitPullRequest />
+            Text
+          </span>
+          TExt
+        </h1>
+        <ul className={click ? "nav-menu active" : "nav-menu"}>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">Search</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
 
-            <div className="btn-container">
-                <button className='btn sn-in'>Sign In</button>
-                <button className='btn sn-up'>Sign Up</button>
-            </div>
+        {/* <div className="btn-container">
+          <button className="btn sn-in">Sign In</button>
+          <button className="btn sn-up">Sign Up</button>
+        </div> */}
 
-            <div className='hamburger' onClick={()=>toggleMenu()}>
-                { click ? (<FaRegTimesCircle/>) : (<HiOutlineMenuAlt4/>)}
-            </div>
+        <div className="hamburger" onClick={() => toggleMenu()}>
+          {click ? <FaRegTimesCircle /> : <HiOutlineMenuAlt4 />}
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,45 +1,41 @@
-import React from 'react'
+import React from "react";
 
-import './Featured.css'
+import "./Featured.css";
 
-import house1 from '../../assets/house1.jpg'
-import bed1 from '../../assets/bed1.jpg'
-import bed2 from '../../assets/bed2.jpg'
-import kitchen from '../../assets/kitchen.jpg'
-import bath1 from '../../assets/bath1.jpg'
+import house1 from "../../assets/house1.jpg";
+import bed1 from "../../assets/bed1.jpg";
+import bed2 from "../../assets/bed2.jpg";
+import kitchen from "../../assets/kitchen.jpg";
+import bath1 from "../../assets/bath1.jpg";
 
-import house2 from '../../assets/house2.jpg'
-import bed3 from '../../assets/bed3.jpg'
-import bed4 from '../../assets/bed4.jpg'
-import bath2 from '../../assets/bath2.jpg'
-import LivingRoom from '../../assets/living-room.jpg'
-
+import house2 from "../../assets/house2.jpg";
+import bed3 from "../../assets/bed3.jpg";
+import bed4 from "../../assets/bed4.jpg";
+import bath2 from "../../assets/bath2.jpg";
+import LivingRoom from "../../assets/living-room.jpg";
 
 const Featured = () => {
   return (
     <div className="featured">
-
-      <h1 className="featured-heading">
-          Top Featured Listings
-      </h1>
+      <h1 className="featured-heading">Top Featured Listings</h1>
       <p className="featured-text">
-          Selected listings by City, State, & Zip based on views
+        Selected listings by City, State, & Zip based on views
       </p>
 
       <div className="container">
-          <img className='span-3 image-grid-row-2' src={house1} alt="" />
-          <img src={bed1} alt="" />
-          <img src={bed2} alt="" />
-          <img src={kitchen} alt="" />
-          <img src={bath1} alt="" />
-          
+        <img className="span-3 image-grid-row-2" src={house1} alt="" />
+        <img src={bed1} alt="" />
+        <img src={bed2} alt="" />
+        <img src={kitchen} alt="" />
+        <img src={bath1} alt="" />
+
         <div className="span-3 left-img-details">
           <div className="top">
             <p>123 Acme St. Dallas, TX</p>
             <p>House for sale</p>
-            <p className='price'>$2,677,000</p>
+            <p className="price">$2,677,000</p>
           </div>
-          
+
           <div className="info-grid">
             <div>
               <div className="info">
@@ -58,39 +54,30 @@ const Featured = () => {
               </div>
             </div>
           </div>
-
         </div>
-      
+
         <div className="span-2 right-img-details">
-          <p>
+          {/* <p>
           A beautiful modern day home in the city with a full-size pool. Spacious and luxurious home located in Texas. Including media room, workout facility, and built-in sauna.
-          </p>
-          <button className='btn'>View listing</button>
+          </p> */}
+          <button className="btn">View listing</button>
         </div>
-      
       </div>
-
-
-
-
-
-
-
 
       <div className="container">
-          <img className='span-3 image-grid-row-2' src={house2} alt="" />
-          <img src={bed3} alt="" />
-          <img src={bed4} alt="" />
-          <img src={bath2} alt="" />
-          <img src={LivingRoom} alt="" />
-          
+        <img className="span-3 image-grid-row-2" src={house2} alt="" />
+        <img src={bed3} alt="" />
+        <img src={bed4} alt="" />
+        <img src={bath2} alt="" />
+        <img src={LivingRoom} alt="" />
+
         <div className="span-3 left-img-details">
           <div className="top">
             <p>123 Acme St. Dallas, TX</p>
             <p>House for sale</p>
-            <p className='price'>$2,677,000</p>
+            <p className="price">$2,677,000</p>
           </div>
-          
+
           <div className="info-grid">
             <div>
               <div className="info">
@@ -109,28 +96,36 @@ const Featured = () => {
               </div>
             </div>
           </div>
+        </div>
 
-        </div>
-      
         <div className="span-2 right-img-details">
-          <p>
-          A beautiful modern day home in the city with a full-size pool. Spacious and luxurious home located in Texas. Including media room, workout facility, and built-in sauna.
-          </p>
-          <button className='btn'>View listing</button>
+          {/* <p>
+            A beautiful modern day home in the city with a full-size pool.
+            Spacious and luxurious home located in Texas. Including media room,
+            workout facility, and built-in sauna.
+          </p> */}
+          <button className="btn">View listing</button>
         </div>
-      
       </div>
 
-
-
-<br /><br /><br /><br /><br /><br /><br /><br />
-<br /><br /><br /><br /><br /><br /><br /><br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
     </div>
-  )
-}
+  );
+};
 
-export default Featured
-
-
-
-
+export default Featured;
